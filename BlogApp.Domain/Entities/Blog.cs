73e2d1b5154155e9eed2956a1f09dcc38a9ed56a
kristapspriede <kristaps.priede@gmail.com>
2021-11-20 20:37:26 +0200

    Created BlogApp

---
+++ BlogApp.Domain/Entities/Blog.cs
@@ -0,0 +1,14 @@
+﻿using BlogApp.Domain.Entities.Abstract;
+
+namespace BlogApp.Domain.Entities
+{
+    public class Blog : EntityBase
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ BlogApp.Domain/Entities/Post.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Domain.Entities.Abstract;
+
+namespace BlogApp.Domain.Entities
+{
+    public class Post : EntityBase
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
